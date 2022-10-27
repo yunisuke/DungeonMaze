2ca52485527f97908122d060d869a3375aab5c91
Takayuki Yoshizawa <>
2022-10-28 01:50:14 +0900

    解像度によらず描画範囲が一定になるように改修

---
+++ Assets/Scripts/AdjustCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustCamera : MonoBehaviour
+{
+    [SerializeField] private float baseWidth = 9.0f;
+    [SerializeField] public float baseHeight = 16.0f;
+
+    void Awake()
+    {
+        var camera = GetComponent<Camera>();
+
+        // アスペクト比固定
+        var scale = Mathf.Min(Screen.height / this.baseHeight, Screen.width / this.baseWidth);
+        var width = (this.baseWidth * scale) / Screen.width;
+        var height = (this.baseHeight * scale) / Screen.height;
+        camera.rect = new Rect((1.0f - width) * 0.5f, (1.0f - height) * 0.5f, width, height);
+    }
+}
